8b2f73df0534dfc0ae2add29e73c7ae88d815150
tuyenleTX <ttle@miu.edu>
2025-07-18 16:07:39 -0500

    add footer test flow & testng basic

--- src/test/java/tests/global/footer/FooterTest.java
+++ src/test/java/tests/global/footer/FooterTest.java
@@ -4,24 +4,55 @@ import driver.DriverFactory;
 import models.components.global.footer.*;
 import models.pages.HomePage;
 import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import test_flows.global.FooterTestFlow;
+import url.Urls;
 
 public class FooterTest {
-    public static void main(String[] args) {
+
+    @Test(priority = 1, dependsOnMethods = {"testFooterRegisterPage"})
+    public void testFooterCategoryPage() {
         WebDriver driver = DriverFactory.getChromeDriver();
-        try {
-            testFooterHomePage(driver);
-            testFooterCategoryPage(driver);
-            testFooterRegisterPage(driver);
-            testFooterLoginPage(driver);
-        }catch (Exception e) {
-            e.printStackTrace();
-        }
-        driver.quit();
+        driver.get(Urls.demoBaseUrl);
+        FooterTestFlow footerTestFlow = new FooterTestFlow(driver);
+        footerTestFlow.verifyFooterComponent();
+    }
+
+    @Test(priority = 2)
+    public void testFooterRegisterPage() {
+        String actualResult = "Teo";
+        String expectedResult = "Ti";
+//        Verifier.verifyEquals(actualResult, expectedResult);
+
+        // Hard assertion
+        Assert.assertEquals(actualResult, expectedResult, "[ERR] Welcome message is incorrect!");
+
+        System.out.println("Hello");
+        Assert.assertTrue(actualResult.equals(expectedResult), "....");
+        Assert.assertFalse(actualResult.equals(expectedResult), "....");
+        Assert.fail();
+        Assert.fail("....");
 
     }
 
-    private static void testFooterHomePage(WebDriver driver) {
-        driver.get("https://demowebshop.tricentis.com/");
+    @Test(priority = 3)
+    public void testFooterLoginPage() {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertEquals(1, 2);
+        softAssert.assertEquals(true, true);
+        softAssert.assertEquals(2, 3);
+
+        softAssert.assertAll();
+
+        System.out.println("hello");
+    }
+
+    @Test(priority = 4)
+    public void testFooterHomePage() {
+        WebDriver driver = DriverFactory.getChromeDriver();
+        driver.get(Urls.demoBaseUrl);
         HomePage homePage = new HomePage(driver);
         InformationColumnComponent informationColumnComponent = homePage.footerComponent().informationColumnComponent();
         testFooterColumn(informationColumnComponent);
@@ -34,16 +65,10 @@ public class FooterTest {
 
         FollowUsColumnComponent followUsColumnComponent = homePage.footerComponent().followUsColumnComponent();
         testFooterColumn(followUsColumnComponent);
+        driver.quit();
     }
 
-    private static void testFooterCategoryPage(WebDriver driver) {
-    }
 
-    private static void testFooterRegisterPage(WebDriver driver) {
-    }
-
-    private static void testFooterLoginPage(WebDriver driver) {
-    }
 
     private static void testFooterColumn(FooterColumnComponent footerColumnComponent) {
         System.out.println(footerColumnComponent.headerElem().getText());
